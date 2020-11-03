affa17aa5da51c794d56130708b674b656f33d90
shuvamk <kshuvam564@gmail.com>
2020-11-02 21:29:28 -0800

    Created body sections and installed Typist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import LeftBody from './components/LeftBody';
+import RightBody from './components/RightBody';
+
 
 function App() {
   return (
     <div className="app">
-      
+     <LeftBody />
     </div>
   );
 }
---
+++ src/components/LeftBody.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './LeftBody.css'
+
+function LeftBody() {
+    return (
+        <div className='leftBody'>
+            
+        </div>
+    )
+}
+
+export default LeftBody
