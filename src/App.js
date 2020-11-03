af3181bf0237723ba2290292e3540e3f470579ed
shuvamk <kshuvam564@gmail.com>
2020-11-03 08:26:39 -0800

    Created Me component basic skeleton

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import './App.css';
 import MainDisplay from './components/MainDisplay';
+import Me from './components/Me';
 import Navbar from './components/Navbar';
 import { ThemeContext } from './context/ThemeContext';
 
@@ -14,7 +15,8 @@ function App() {
   return (
     <div className="app">
       <Navbar />
-      <MainDisplay />
+      {/* <MainDisplay /> */}
+      <Me />
     </div>
   );
 }
---
+++ src/components/Me.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Me.css'
+
+function Me() {
+    return (
+        <div className='me'>
+            <div className='me__left'>
+                <img src='https://cdn0.iconfinder.com/data/icons/social-media-network-4/48/male_avatar-512.png' alt='' />
+            </div>
+            <div className='me__right'>
+                <p><span className='me__spanText'>My wisdom</span>: "An apple a day keeps the doctor away"</p>
+                <div className='me__right--about'>
+                    <p><i>I am an engineering student, currently in my third year of college.
+                    I like exploring technology, nature and human psychology. I always try to have an 
+                    entrepreneurial angle in every aspect of life. I like brainstorming ideas and 
+                    bringing them to life. I am noob in this web development domain and I've displayed my skills 
+                    down below. To have a discussion on literaly any topic, drop me a text.</i>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Me
