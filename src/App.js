055f76fcf29a1abf6ac9afb400e0029080be644b
shuvamk <kshuvam564@gmail.com>
2020-11-03 02:34:49 -0800

    Cleaned up unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import LeftBody from './components/LeftBody';
-import RightBody from './components/RightBody';
+
 
 
 function App() {
   return (
     <div className="app">
-     <LeftBody />
+     
     </div>
   );
 }
